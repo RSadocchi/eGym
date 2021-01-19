5e3391f100a3661996794e04d02fce005d1fb03c
Riccardo Sadocchi <r.sadocchi@aran-solutions.com>
2021-01-19 17:58:51 +0100

    domain in progress

--- eGym.Core.Domain/AggregateModels/Anag/Anag_Contact.cs
+++ eGym.Core.Domain/AggregateModels/Anag/Anag_Contact.cs
@@ -34,4 +34,43 @@ namespace eGym.Core.Domain
         }
         #endregion
     }
+
+    public class Anag_Document : Entity
+    {
+        #region Db Columns
+        [Key]
+        public int Doc_ID { get; set; }
+        [Column(TypeName = "datetime")]
+        public DateTime Doc_CreationDate { get; set; }
+        [MaxLength(250)]
+        public string Doc_Description { get; set; }
+        [Column(TypeName = "date")]
+        public DateTime? Doc_IssueDate { get; set; }
+        [Column(TypeName = "date")]
+        public DateTime? Doc_ExpiringDate { get; set; }
+        [MaxLength(100)]
+        public string Doc_Number { get; set; }
+        [Column(TypeName = "nchar(3)")]
+        public string Doc_EmissionCountry { get; set; }
+        [MaxLength(100)]
+        public string Doc_EmissionCity { get; set; }
+        [MaxLength(250)]
+        public string Doc_EmissionNote { get; set; }
+        public string Doc_Path { get; set; }
+        public short Doc_TypeID { get; set; }
+        public short? Doc_EmitterID { get; set; }
+        public int Doc_AnagID { get; set; }
+        #endregion
+
+        #region Virtuals
+        public virtual Anag_Master Anag_Master { get; set; }
+        #endregion
+
+        #region Constructor
+        public Anag_Document()
+        {
+
+        }
+        #endregion
+    }
 }
--- eGym.Core.Domain/Enumeration/EN_Gender.cs
+++ eGym.Core.Domain/Enumeration/EN_Gender.cs
@@ -17,4 +17,22 @@ namespace eGym.Core.Domain
         public static EN_Gender Male = new EN_Gender(1, nameof(Male));
         public static EN_Gender Female = new EN_Gender(2, nameof(Female));
     }
+
+    public partial class EN_DocumentEmitter : Enumeration<short>
+    {
+        public EN_DocumentEmitter() { }
+        public EN_DocumentEmitter(short id, string code) : base(id, code) { }
+
+
+        public static IEnumerable<EN_DocumentEmitter> GetAll() => GetAll<EN_DocumentEmitter>();
+        public static EN_DocumentEmitter FromID(short id) => FromID<EN_DocumentEmitter>(id);
+
+
+        public static EN_DocumentEmitter Municipality = new EN_DocumentEmitter(1, nameof(Municipality));
+        public static EN_DocumentEmitter Prefecture = new EN_DocumentEmitter(2, nameof(Prefecture));
+        public static EN_DocumentEmitter PoliceHeadquarters = new EN_DocumentEmitter(3, nameof(PoliceHeadquarters));
+        public static EN_DocumentEmitter Hospital = new EN_DocumentEmitter(4, nameof(Hospital));
+        public static EN_DocumentEmitter Doctor = new EN_DocumentEmitter(5, nameof(Doctor));
+        public static EN_DocumentEmitter SportsDoctor = new EN_DocumentEmitter(6, nameof(SportsDoctor));
+    }
 }
