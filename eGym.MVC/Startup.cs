f903c77269d6f58837b20e4b07efa2d592cfb650
Riccardo Sadocchi <r.sadocchi@aran-solutions.com>
2021-01-21 18:07:42 +0100

    area admin start

--- eGym.MVC/Startup.cs
+++ eGym.MVC/Startup.cs
@@ -125,6 +125,9 @@ namespace eGym.MVC
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
