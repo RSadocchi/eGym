9ed6ec46e423086a0e7f4863c584f714cf0a350d
Riccardo Sadocchi <r.sadocchi@aran-solutions.com>
2021-02-02 18:05:41 +0100

    in progress

--- eGym.MVC/Helpers/ModalTagHelper.cs
+++ eGym.MVC/Helpers/ModalTagHelper.cs
@@ -19,8 +19,11 @@ namespace eGym.MVC.Helpers
             output.TagName = "div";
             output.Attributes.SetAttribute("class", "modal fade");
             output.Attributes.SetAttribute("id", ID ?? System.Guid.NewGuid().ToString());
+            output.Attributes.SetAttribute("tabindex", "-1");
+            output.Attributes.SetAttribute("role", "dialog");
+            output.Attributes.SetAttribute("aria-hidden", "true");
             var sb = new StringBuilder();
-            sb.AppendLine($@"<div class='modal-dialog modal-dialog-centered modal-{Size.ToString()}'>");
+            sb.AppendLine($@"<div class='modal-dialog modal-dialog-centered modal-{Size.ToString()}' role='document'>");
             sb.AppendLine($@"<div class='modal-content'>");
             output.PreContent.SetHtmlContent(sb.ToString());
             output.Content.SetHtmlContent(output.GetChildContentAsync().Result.GetContent());
@@ -69,6 +72,32 @@ namespace eGym.MVC.Helpers
         /// Default "modal-body", "modal-body-scrollable"
         /// </summary>
         public List<string> CssBody { get; set; } = new List<string>() { "modal-body", "modal-body-scrollable" };
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            output.Attributes.SetAttribute("class", $"{string.Join(" ", CssBody)}");
+            if (!string.IsNullOrWhiteSpace(ID)) output.Attributes.SetAttribute("id", ID);
+            var sb = new StringBuilder();
+            output.PreContent.SetHtmlContent(sb.ToString());
+            output.Content.SetHtmlContent(output.GetChildContentAsync().Result.GetContent());
+            output.TagMode = TagMode.StartTagAndEndTag;
+        }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            Process(context, output);
+            await Task.CompletedTask;
+        }
+    }
+
+    public class ModalFooterTagHelper : TagHelper
+    {
+        public string ID { get; set; }
+        /// <summary>
+        /// Default "modal-footer"
+        /// </summary>
+        public List<string> CssBody { get; set; } = new List<string>() { "modal-footer" };
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
