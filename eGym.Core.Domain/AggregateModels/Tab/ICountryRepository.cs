feba0f5a0cf3096983ad6b2fb3376014272fd9da
Riccardo Sadocchi <r.sadocchi@aran-solutions.com>
2021-02-14 20:19:32 +0100

    in progress

---
+++ eGym.Core.Domain/AggregateModels/Tab/ICountryRepository.cs
@@ -0,0 +1,15 @@
+﻿using eGym.Core.SeedWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eGym.Core.Domain.AggregateModels
+{
+    
+    public interface ICountryRepository : IRepository<Country, string>
+    {
+        Country this[string isoCode] { get; }
+    }
+}
